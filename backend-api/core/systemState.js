5745422268164f5c6fb39bfacc1b0d36d2a00820
richardnguyen <richardnguyen888@yahoo.com>
2025-11-21 20:56:58 -0800

    Update systemState.js

--- backend-api/core/systemState.js
+++ backend-api/core/systemState.js
@@ -1,1 +1,31 @@
+// backend-api/core/systemState.js
+
+class SystemState {
+  constructor() {
+    this.state = {
+      scanner: {},
+      signals: [],
+      lastUpdate: null,
+    };
+  }
+
+  updateScanner(data) {
+    this.state.scanner = data;
+    this.state.lastUpdate = Date.now();
+  }
+
+  addSignal(signal) {
+    this.state.signals.push({
+      ...signal,
+      timestamp: Date.now(),
+    });
+  }
+
+  getState() {
+    return this.state;
+  }
+}
+
+const systemState = new SystemState();
+export default systemState;
 
